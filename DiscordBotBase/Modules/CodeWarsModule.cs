12110476af08949fa5fa3385cc3541ce4e14ad08
rese-sw <22599403+rese-sw@users.noreply.github.com>
2019-04-17 20:34:58 +0200

    Update CodeWarsModule.cs
    fixed structure

--- DiscordBotBase/Modules/CodeWarsModule.cs
+++ DiscordBotBase/Modules/CodeWarsModule.cs
@@ -1,4 +1,4 @@
-﻿using Discord;
+using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using System.Threading.Tasks;
@@ -13,57 +13,41 @@ namespace DiscordBotBase.Modules
 {
     public class CodeWarsModule : ModuleBase<SocketCommandContext>
     {
-        // This is ugly, i know!
-        private readonly Utility utility = new Utility();
+        private static readonly Utility utility = new Utility();
 
-        [Command("cw get user"), Alias("cwgusr")]
-        [Summary("Fetches information about a supplied user from codewars")]
-        [RequireUserPermission(GuildPermission.SendMessages)]
-        public async Task UserValuesAsync([Remainder]string text)
-        {
-            try
-            {
-                var Skills = string.Empty;
-                var LanguageRank = string.Empty;
-
-                var response = utility.ExecuteRequest($"{text}", "https://www.codewars.com/api/v1/users/", RestSharp.Method.GET);
-
-                foreach (var item in response.Skills)
-                {
-                    Skills += $"{item}, ";
-                }
-
-                foreach (var item in response.Ranks.Languages)
-                {
-                    LanguageRank += $"{item.Key} : {item.Value.Name}({item.Value.Score}), ";
-                }
-
-                await ReplyAsync($"```Username: {response.Username}{Environment.NewLine}Honor: {response.Honor}{Environment.NewLine}CompletedChallenges: {response.CodeChallenges.TotalCompleted}{Environment.NewLine}" +
-                    $"Skills: {Skills}{Environment.NewLine}LanguageRank: {LanguageRank}```");
-            }
-            catch
-            {
-                await ReplyAsync($"`Sorry something went wrong!`");
-            }
-        }
-
-        [Group("set"), Name("Example")]
+        [Group("cw"), Name("CodeWars")]
         [RequireContext(ContextType.Guild)]
-        public class Set : ModuleBase
+        public class CW : ModuleBase
         {
-            [Command("nick"), Priority(1)]
-            [Summary("Change your nickname to the specified text")]
-            [RequireUserPermission(GuildPermission.ChangeNickname)]
-            public Task Nick([Remainder]string name)
-                => Nick(Context.User as SocketGuildUser, name);
-
-            [Command("nick"), Priority(0)]
-            [Summary("Change another user's nickname to the specified text")]
-            [RequireUserPermission(GuildPermission.ManageNicknames)]
-            public async Task Nick(SocketGuildUser user, [Remainder]string name)
+            [Command("get user"), Alias("gusr")]
+            [Summary("Fetches information about a supplied user from codewars")]
+            [RequireUserPermission(GuildPermission.SendMessages)]
+            public async Task UserValuesAsync([Remainder]string text)
             {
-                await user.ModifyAsync(x => x.Nickname = name);
-                await ReplyAsync($"{user.Mention} I changed your name to **{name}**");
+                try
+                {
+                    var Skills = string.Empty;
+                    var LanguageRank = string.Empty;
+
+                    var response = utility.ExecuteRequest($"{text}", "https://www.codewars.com/api/v1/users/", RestSharp.Method.GET);
+
+                    foreach (var item in response.Skills)
+                    {
+                        Skills += $"{item}, ";
+                    }
+
+                    foreach (var item in response.Ranks.Languages)
+                    {
+                        LanguageRank += $"{item.Key} : {item.Value.Name}({item.Value.Score}), ";
+                    }
+
+                    await ReplyAsync($"```Username: {response.Username}{Environment.NewLine}Honor: {response.Honor}{Environment.NewLine}CompletedChallenges: {response.CodeChallenges.TotalCompleted}{Environment.NewLine}" +
+                        $"Skills: {Skills}{Environment.NewLine}LanguageRank: {LanguageRank}```");
+                }
+                catch
+                {
+                    await ReplyAsync($"`Sorry something went wrong!`");
+                }
             }
         }
     }
